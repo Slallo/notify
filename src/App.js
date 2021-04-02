4001a6f57f8020f41741c8f90c2f7e9c05bc65ef
Francesco Lupo <fra.lupo.91@icloud.com>
2021-04-02 23:51:21 +0200

    - First commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function isPushNotificationSupported() {
+  return "serviceWorker" in navigator && "PushManager" in window;
+}
+
+async function askUserPermission() {
+  return await Notification.requestPermission();
+}
+
+askUserPermission();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{isPushNotificationSupported ? "Support" : "No notify support"}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
